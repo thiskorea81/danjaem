807a8811571f980ad34f530e0094f8aa312cb3b1
Taehun Lee <thiskorea@cberi.go.kr>
2024-01-01 22:23:07 +0900

    프론트엔드 구조 설정

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ from routers.users import router as users_router
 from routers.auth import router as auth_router
 from database import engine
 from models import Base
+
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(auth_router)
 
 Base.metadata.create_all(bind=engine)
+
 # 'frontend' 폴더 내의 정적 파일 제공
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
-# 나머지 애플리케이션 코드...
+
+# HTML 템플릿을 위한 설정
+templates = Jinja2Templates(directory="frontend/html")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
